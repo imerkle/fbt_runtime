5fa97bd8f56c39228587bf4028d01f8505d17a3c
slim <dsslimshaddy@gmail.com>
2019-01-20 02:38:08 +0530

    parent add

--- src/nonfb/FbtTranslations.js
+++ src/nonfb/FbtTranslations.js
@@ -12,7 +12,7 @@
 
 const IntlViewerContext = require('IntlViewerContext');
 
-const translatedFbts = require('translatedFbts.json');
+const translatedFbts = module.parent.require('./translatedFbts.json');
 
 function getTranslatedPayload(
   hashKey: ?string,
